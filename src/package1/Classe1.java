a210f66a22ad8018e9cae5aeb6c95c9ed8db8f36
Enzo <Enzo@DESKTOP-0P75JRG>
2021-07-21 15:27:03 +0200

    ok

---
+++ src/package1/Classe1.java
@@ -0,0 +1,16 @@
+package package1;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Classe1 {
+
+	private static final Logger log = LogManager.getLogger(Classe1.class);
+	
+	public static void main(String[] args) {
+		log.debug("prova");
+		}
+		
+	}
+
---
+++ src/package1/Classe1Test.java
@@ -0,0 +1,17 @@
+package package1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+class Classe1Test {
+
+	@Test
+	void test() {
+		
+	}
+
+}
